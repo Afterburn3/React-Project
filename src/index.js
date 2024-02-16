fef0d3ec115cd33ff40602aa127f67030b34ab2a
Kim Seng Thai <148199726+Afterburn3@users.noreply.github.com>
2024-02-16 14:51:43 +1100

    react

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <>
+    <h1>hello world</h1>
+    <p>hello</p>
+  </>,
+  document.getElementById("root")
+);
